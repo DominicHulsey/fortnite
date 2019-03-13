18fc67654c6984ba80ed0155f98fc1a778cf75c1
Dominic Hulsey <dominic.hulsey.dh@gmail.com>
2019-03-13 10:14:42 -0600

    connected api

--- src/store.js
+++ src/store.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import bootstrap from 'bootstrap'
 
 Vue.use(Vuex)
 
+let api = axios.create({
+  baseURL: "https://fortnite-public-api.theapinetwork.com/prod09/challenges/get?season=current"
+})
+
 export default new Vuex.Store({
   state: {
-
+    challenges: []
   },
   mutations: {
-
+    setChallenges(state, data) {
+      state.challenges = data
+    }
   },
   actions: {
-
+    getChallenges({ commit, dispatch }) {
+      api.get('')
+        .then(res => {
+          commit('setChallenges', res.data.challenges)
+        })
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="@/assets/galaxy.png" />
+    <table class="table table-striped table-dark">
+      <thead class="">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Challenge</th>
+          <th scope="col">Total required</th>
+          <th scope="col">Stars</th>
+          <th scope="col">Difficulty</th>
+        </tr>
+      </thead>
+      <tbody v-for="(challenge, index) in challenges">
+        <tr>
+          <th scope="row">{{index + 1}}</th>
+          <td>{{challenge.challenge}}</td>
+          <td>{{challenge.total}}</td>
+          <td>{{challenge.stars}}</td>
+          <td>{{challenge.difficulty}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    computed: {
+      challenges() {
+        return this.$store.state.challenges.week1
+      }
+    },
+    components: {
+
+    }
   }
-}
 </script>
